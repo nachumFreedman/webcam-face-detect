f5d2e48452f6f5f04650ca71d061485babc2fad7
nachumyf@gmail.com <nachumyf@gmail.com>
2019-01-06 19:30:34 +0200

    app does do stuff server not up no css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Camera from './Camera';
+
+//const domain = 'https://facedetec.herokuapp.com';
+const domain = 'http://localhost:3000';
 
 class App extends Component {
+  state = {
+    posts: [],
+    isFaceAttempts: 0,
+    faces: []    
+  }
+
+  
+  checkFace = () => (this.state.faces.length ? null :
+                     setTimeout(()=>{this.sendFace() &&
+                                     this.setState({isFaceAttemps: this.state.isFaceAttempts + 1})},2000)
+  )
+  
+  setImg = img => {
+    console.log(img)
+    const imgFile = this.dataURLtoFile(img, 'image.jpg');  
+    console.log(imgFile)
+    const formData  = new FormData();
+    
+    formData.append('file', imgFile);
+
+    fetch(domain+'/upload', {
+        method: 'POST',
+        body: formData
+    }).then((response) => response.json()
+    ).then((faces)=>this.setState({faces}) && console.log(faces));     
+  }
+  
+
+  dataURLtoFile = (dataurl, filename) => {
+    let arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+    while(n--){
+      u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new File([u8arr], filename, {type:mime});
+  }
+
+  
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Camera onImg={this.setImg} isFace={this.state.isFace}/>
+        {
+          this.state.isFaceAttempts > 2 ?
+                                      <h1>No face</h1>
+                                    : null
+        }
       </div>
     );
   }
